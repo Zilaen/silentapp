3f034dfc05f6d250cb4a3b3e9e620c71e7f4ff33
Zilaen <anvasmos@gmail.com>
2013-02-10 19:58:32 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Silentapp::Application.config.secret_token = '54f6ffca5d3e7a720d06c7a34501836f447ccae226f3a2ce1c518cb3b8b957f7b2f527022013f9d913e7f79bf4888312d678dc9bf5d965c0b4a0d61c006eda9d'
